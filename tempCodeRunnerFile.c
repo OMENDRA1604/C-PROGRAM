c12c9ee82eb627cdd25c8fdd5658d9ebca3f2f0a
OMENDRA1604 <omendrasingh1604@gmail.com>
2024-05-26 16:02:00 +0530

    ADD NEW PROGRAM

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,16 @@
-n
+int lengthOfLastWord(char *s)
+{
+    int n = strlen(s), count = 0, i = n - 1;
+    if (n == 1)
+        return 1;
+    for (int j = n - 1; *(s + j) == ' '; j--)
+    {
+        i--;
+    }
+    while (*(s + i) != ' ')
+    {
+        count++;
+        i--;
+    }
+    return count;
+}
